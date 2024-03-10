c49af7603accea06318915ee458fe9bbefaf9ea7
abimanyugithub <abimanyubima@hotmail.com>
2024-03-10 11:36:27 +0700

    Re-Upload Repository IAS

---
+++ 328/include/setarrow.js
@@ -0,0 +1,34 @@
+// ---------------- variabel ambil tanda panah dari slice lokasi  ----------------
+var vararrow={
+	
+	// ---------------- copas lokasi & voice digit disini, pastikan braket "}" diakhir variabel ini ----------------
+AB:"&#x2B06;",
+AC:"&#x2B06;",
+AD:"&#x2B06;",
+AA:"&#x2B07;",
+BA:"&#x2B07;",
+BB:"&#x2B06;",
+BC:"&#x2B06;",
+BD:"&#x2B06;",
+CA:"&#x2B07;",
+CB:"&#x2B06;",
+CC:"&#x2B06;",
+CD:"&#x2B06;",
+CE:"&#x2B06;",
+EA:"&#x2B07;",
+EB:"&#x2B06;",
+EC:"&#x2B06;",
+ED:"&#x2B06;",
+FA:"&#x2B07;",
+FB:"&#x2B06;",
+FC:"&#x2B06;",
+FD:"&#x2B06;",
+MA:"&#x2B07;",
+SA:"&#x2B07;",
+VA:"&#x2B07;",
+VB:"&#x2B06;",
+VC:"&#x2B06;",
+VD:"&#x2B06;",
+	// arah bawah &#x2B07;
+	// arah atas &#x2B06;
+}
---
+++ 365/include/setarrow.js
@@ -0,0 +1,24 @@
+// ---------------- variabel ambil tanda panah dari slice lokasi  ----------------
+var vararrow={
+	
+	// ---------------- copas lokasi & voice digit disini, pastikan braket "}" diakhir variabel ini ----------------
+	AA:"&#x2B07;", 
+	AB:"&#x2B06;",
+	AE:"&#x2B06;",
+	BA:"&#x2B07;", 
+	BB:"&#x2B06;",
+	BC:"&#x2B06;",
+	BD:"&#x2B06;",
+	TA:"&#x2B07;",
+	TB:"&#x2B06;",
+	TC:"&#x2B06;",
+	TD:"&#x2B06;",
+	SA:"&#x2B07;",
+	SB:"&#x2B06;",
+	LA:"&#x2B07;",
+	LB:"&#x2B06;",
+	LC:"&#x2B06;",
+	LD:"&#x2B06;",
+	// arah bawah &#x2B07;
+	// arah atas &#x2B06;
+}
